d949aef463098044f225dbacd42a073e8eff4082
Bogdan Lytvyn <boddofish@gmail.com>
2021-03-04 19:59:34 +0200

    DOne

---
+++ src/js/custom.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+  // Animated numbers Caunter
+    var show = true;
+    var countbox = ".how-it-made__number";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 830 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.how-it-made__number').css('opacity', '1');
+            $('.how-it-made__number').spincrement({
+                thousandSeparator: "",
+                duration: 2500
+            });
+
+            show = false;
+        }
+    });
+
+    // Lite scroll navigation for page
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+});
+
+// The button Back to top
+
+var btn = $('#button');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
